48f8c5efc1cde95ba70de216c2b5f2a4d4a1fa68
George-Abboud <geo1111ang@gmail.com>
2025-08-30 03:39:13 +0300

    fixing issues

--- lib/Features/AI Assistant/Presentation/Views/WIdgets/ai_home_trips_view_body.dart
+++ lib/Features/AI Assistant/Presentation/Views/WIdgets/ai_home_trips_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:visit_syria/Core/constants/text_constant.dart';
+import 'package:visit_syria/Core/utils/app_router.dart';
 import 'package:visit_syria/Core/widgets/custom_app_bar_2.dart';
 import 'package:visit_syria/Features/AI%20Assistant/Presentation/Views/WIdgets/ai_floating_action_button_builder.dart';
 import 'package:visit_syria/Features/AI%20Assistant/Presentation/Views/WIdgets/ai_home_trips_builder.dart';
@@ -10,7 +12,12 @@ class AIHomeTripsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar2(title: kAIAssistantAppBarTitle),
+      appBar: CustomAppBar2(
+        title: kAIAssistantAppBarTitle,
+        onTap: () {
+          GoRouter.of(context).goNamed(AppRouter.kAppRootName);
+        },
+      ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(right: 16, bottom: 40),
         child: AiFloatingActionButtonBuilder(),
